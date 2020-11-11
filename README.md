d48a0cec6476776b903cda3c58f93fbe6379cd95
Abhinav <abhinavkarn55@gmail.com>
2020-11-11 22:31:13 +0545

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# What is this used for ?
+
+Get a perfect shadow to your div or image
+
+# Installation
+
+`npm i Boxshadow --save`
+
+
+Then...
+
+```
+import {Boxshadow} from 'BoxShadow';
+
+Boxshadow({
+    shadow_type: 'soft',
+    padding: false
+});
+```
---
+++ index.js
@@ -0,0 +1,21 @@
+function Boxshadow (options) {
+    let images  = document.querySelectorAll('.boxshadow');
+
+    if(options.shadow_type == 'hard') {
+        options.shadow_type = '0px'
+    } else {
+        options.shadow_type = '15px'
+    }
+
+    images.forEach(image => {
+        image.style.boxShadow = `10xp 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)`;
+
+
+        if(options.padding) {
+            image.style.padding = '1em'
+        }
+
+    })
+}
+
+module.exports.Boxshadow = Boxshadow;
